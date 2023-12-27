4484f00135b7221ee814728307724c7fbdf2a852
Munteanu Alexandru <103954658+AlexandruMunteanuIoan@users.noreply.github.com>
2023-12-27 12:19:19 +0200

    Update web.php
    Add auth

--- food-blog/routes/web.php
+++ food-blog/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\FoodBlogController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,20 +27,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/blog/create',[FoodBlogController::class,'create'])->name('create');
+    Route::post('/blog/store',[FoodBlogController::class,'store'])->name('store');
+    Route::get('blog/edit/{blog}', [FoodBlogController::class,'edit'])->name('edit');
+    Route::patch('blog/update/{blog}',[FoodBlogController::class,'update'])->name('update');
+    Route::delete('blog/delete/{blog}',[FoodBlogController::class,'delete'])->name('delete');
 });
 
 require __DIR__.'/auth.php';
 
 Route::get('/blog', [FoodBlogController::class,'blog'])->name('blog');
-
-Route::get('/blog/create',[FoodBlogController::class,'create'])->name('create');
-
-Route::post('/blog/store',[FoodBlogController::class,'store'])->name('store');
-
-Route::get('blog/edit/{blog}', [FoodBlogController::class,'edit'])->name('edit');
-
-Route::patch('blog/update/{blog}',[FoodBlogController::class,'update'])->name('update');
-
 Route::get('blog/read/{blog}',[FoodBlogController::class,'read'])->name('read');
 
-Route::delete('blog/delete/{blog}',[FoodBlogController::class,'delete'])->name('delete');
